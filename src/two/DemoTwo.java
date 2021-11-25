eabd799dd8162eaf1d692f8deca6f148a09c4dfd
ambueno323 <94875424+ambueno323@users.noreply.github.com>
2021-11-24 20:46:53 -0500

    Update DemoTwo.java
    committed change

--- src/two/DemoTwo.java
+++ src/two/DemoTwo.java
@@ -4,7 +4,7 @@ public class DemoTwo {
 	public static void main(String[] args) {
 		System.out.println("DEMO TWO");
 		
-		System.out.println(" THIS IS FROM GITHUB, THIS IS NOT A CHANGE BUT A COMMENT" );
+		System.out.println("THIS IS FROM GITHUB, THIS IS NOT A CHANGE BUT A COMMENT");
 	}
 
 	public static void addNum(int a, int b) {
